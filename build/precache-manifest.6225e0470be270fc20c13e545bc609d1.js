5e0469d403795b3f3670573713344abec81c6f0c
rajatrai30 <mailmerairajat@gmail.com>
2022-12-09 18:15:28 +0530

    build commit

---
+++ build/precache-manifest.6225e0470be270fc20c13e545bc609d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c04eb6c7588d11937b7c",
+    "url": "/static/css/main.a256d70f.chunk.css"
+  },
+  {
+    "revision": "c04eb6c7588d11937b7c",
+    "url": "/static/js/main.29f635b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d90fa1f39a1d658acfb",
+    "url": "/static/js/2.13403f2b.chunk.js"
+  },
+  {
+    "revision": "31fdb75b09a2cd1d53e3a909c0bebc32",
+    "url": "/index.html"
+  }
+];
